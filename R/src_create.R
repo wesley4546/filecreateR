493fb11c91be2711d1c2e6a85c2e757947aad0b9
Wesley G <56905659+wesley4546@users.noreply.github.com>
2020-03-09 19:38:29 -0400

    Update src_create.R

--- R/src_create.R
+++ R/src_create.R
@@ -9,15 +9,15 @@
 src_create <- function(Rsub = FALSE){
 
   if(Rsub == TRUE){
-    print("Creating src folder...")
+
     #creates a source file with R subfolder
     dir.create(here::here("src"),recursive = TRUE)
+
     #creates a source file with R subfolder
-    print("Creating R subfolder...")
     dir.create(here::here("src", "R"),recursive = TRUE)
   }
   else{
-    print("Creating src folder...")
+
     #creates a source file with R subfolder
     dir.create(here::here("src"),recursive = TRUE)
   }
